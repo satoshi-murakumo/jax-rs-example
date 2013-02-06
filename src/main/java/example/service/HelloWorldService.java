900b43f2be46e2271425835deaa31ddec184af8f
satoshi-murakumo <satoshi.murakumo@gmail.com>
2013-02-06 20:32:59 +0900

    add Google Guice support.
    HelloWorldInterface was removed, Because redundant.

--- src/main/java/example/service/HelloWorldService.java
+++ src/main/java/example/service/HelloWorldService.java
@@ -3,17 +3,59 @@
  */
 package example.service;
 
-public class HelloWorldService implements HelloWorldInterface {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-    /* (非 Javadoc)
-     * @see example.service.HelloWorldInterface#get(java.lang.String)
-     */
-    @Override
-    public HelloResponse get(String message) {
+import org.jboss.resteasy.annotations.Suspend;
+import org.jboss.resteasy.spi.AsynchronousResponse;
 
-        HelloResponse result = new HelloResponse("accept message from client", message);
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+@Path("/hello")
+public class HelloWorldService {
+
+    @Inject
+    @Named("opning message")
+    protected String opningMessage;
+
+    @GET
+    @Path("/world/{param}")
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public HelloResponse get(final @PathParam("param") String message) {
+
+        HelloResponse result = new HelloResponse(opningMessage, message);
 
         return result;
+    }
+
+    @GET
+    @Path("/worldasync")
+    @Produces(MediaType.TEXT_PLAIN)
+    public void getAsync(final @Suspend(10000) AsynchronousResponse response) {
+        // XXX: this method not work
+
+        Thread t = new Thread()
+        {
+           @Override
+           public void run()
+           {
+              try
+              {
+                 Response jaxrs = Response.ok("(message)").type(MediaType.TEXT_PLAIN).build();
+                 response.setResponse(jaxrs);
+              }
+              catch (Exception e)
+              {
+                 e.printStackTrace();
+              }
+           }
+        };
+        t.start();
     }
 
 }
